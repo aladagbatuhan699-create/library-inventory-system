ea143de1f580e958b5ce844c87f62dfa7f92dc1a
Damla Örmeci <250601055@bakircay.edu.tr>
2026-05-16 02:37:51 +0300

    kod güncellendi

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>e
+
+
+int main()
+{
+    char key,damla;
+    printf("a  print button");
+    key=getch();
+    printf("%c you write\n",key);
+damla=putchar(65);
+    return 0;
+}
